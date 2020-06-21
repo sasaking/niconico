db30b7fd9e97541b726ab59568f57181d43d2692
Ubuntu <ubuntu@ip-172-31-38-19.us-east-2.compute.internal>
2020-06-21 10:14:44 +0000

    create development enviroment

--- config/application.rb
+++ config/application.rb
@@ -10,6 +10,9 @@ module Niconico
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.0
+    
+    #add_sasaki
+    config.hosts << 'fd88bfe81e674c25a665778413685c7d.vfs.cloud9.us-east-2.amazonaws.com'
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
